59772253b3e76a4c2ec685098c557df893650692
Jarvis Yeh <jarvisyeh@qq.com>
2021-08-07 17:35:51 +0800

    update bst

---
+++ src/BST/Test653_TwoSumIVInputBST.java
@@ -0,0 +1,74 @@
+package BST;
+
+import util.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+
+public class Test653_TwoSumIVInputBST {
+	// Method 1: recursion
+	// two sum with hashset
+	// return ture/false if bst contains two nodes
+	// sum(n1.key, n2.key) == k
+	// traverse bst and use hashmap
+	// TC: O(n)
+	// SC: O(n)
+	public boolean findTargetI(TreeNode root, int k) {
+		HashSet<Integer> set = new HashSet<>();
+		return inOrder(root, set, k);
+	}
+
+	private boolean inOrder(TreeNode root, HashSet<Integer> set, int k) {
+		if (root == null) return false;
+		if (set.contains(k - root.key)) return true;
+
+		set.add(root.key);
+		return inOrder(root.left, set, k) || inOrder(root.right, set, k);
+	}
+
+	// Method 2: iteration
+	// two sum with two pointers
+	// TC: O(n)
+	// SC: O(n)
+	public boolean findTargetII(TreeNode root, int k) {
+		// stack 1 for in-order traversal
+		// left - root - right
+		Deque<TreeNode> s1 = new ArrayDeque<>();
+		pushLeftNodes(s1, root);
+		// stack 2 for reverse in-order traversal
+		// right - root - left
+		Deque<TreeNode> s2 = new ArrayDeque<>();
+		pushRightNodes(s2, root);
+
+		// same as two pointers while (i < j)
+		while (!s1.isEmpty() && !s2.isEmpty() && s1.peekFirst() != s2.peekFirst()) {
+			if (s1.peekFirst().key + s2.peekFirst().key > k) {
+				TreeNode curr = s2.pollFirst();
+				pushRightNodes(s2, curr.left);
+			} else if (s1.peekFirst().key + s2.peekFirst().key < k) {
+				TreeNode curr = s1.pollFirst();
+				pushLeftNodes(s1, curr.right);
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+
+	}
+
+	private void pushLeftNodes(Deque<TreeNode> s, TreeNode root) {
+		while (root != null) {
+			s.offerFirst(root);
+			root = root.left;
+		}
+	}
+
+	private void pushRightNodes(Deque<TreeNode> s, TreeNode root) {
+		while (root != null) {
+			s.offerFirst(root);
+			root = root.right;
+		}
+	}
+}
---
+++ src/BST/Test99_RecoverBinarySearchTree.java
@@ -0,0 +1,93 @@
+package BST;
+
+import util.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Test99_RecoverBinarySearchTree {
+	// method 1: recursion
+	public void recoverTree(TreeNode root) {
+		TreeNode[] misNodes = new TreeNode[2];
+		TreeNode[] prev = {new TreeNode(Integer.MIN_VALUE)};
+
+		inOrder(root, prev, misNodes);
+		// swap twp misplaced nodes values
+		int tmp = misNodes[0].key;
+		misNodes[0].key = misNodes[1].key;
+		misNodes[1].key = tmp;
+	}
+
+	private void inOrder(TreeNode root, TreeNode[] prev, TreeNode[] misNodes) {
+		if (root == null) return;
+
+		inOrder(root.left, prev, misNodes);
+		// current level, check relationship with previous nodes
+		// case 1:
+		// haven't found the first misplaced node
+		// the first misplaced node will be larger than the next node
+		// large value moves to front: 1 2 3 '10' 4 ...
+		if (misNodes[0] == null && root.key < prev[0].key) {
+			misNodes[0] = prev[0];
+		}
+
+		// case 2:
+		// found first misplaced node
+		// the second misplaced node will be less than previous node
+		// small value move to back: 7 8 9 '5' 11
+		if (misNodes[0] != null && root.key < prev[0].key) {
+			misNodes[1] = root;
+		}
+		prev[0] = root;
+		inOrder(root.right, prev, misNodes);
+	}
+
+
+	// method 2: iteration
+	public void recover(TreeNode root) {
+		Deque<TreeNode> stack = new ArrayDeque<>();
+		while (root != null) {
+			stack.offerFirst(root);
+			root = root.left;
+		}
+
+		TreeNode first = null, second = null;
+		TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+		while (!stack.isEmpty()) {
+			TreeNode curr = stack.pollFirst();
+			// process curr
+			// curr.key is supposed to be > prev
+			// first misplaced node < next node, large node move forward
+			// second misplaced node > prev node, small node move backward
+			// 1 2 {7} 4 5 6 {3} 8 9
+			if (first == null && curr.key < prev.key) {
+				first = prev;
+			}
+			if (first != null && curr.key < prev.key) {
+				second = curr;
+			}
+
+			TreeNode tmp = curr.right;
+			while (tmp != null) {
+				stack.offerFirst(tmp);
+				tmp = tmp.left;
+			}
+			// update prev
+			prev = curr;
+		}
+
+		int tmp = first.key;
+		first.key = second.key;
+		second.key = tmp;
+	}
+
+	public static void main(String[] args) {
+		Test99_RecoverBinarySearchTree t = new Test99_RecoverBinarySearchTree();
+		TreeNode root = TreeNode.formTreeByLayer(new Integer[]{5,7,9,null,4,3});
+		TreeNode.printTree(root);
+		t.recover(root);
+		TreeNode.printTree(root);
+
+	}
+}
+
